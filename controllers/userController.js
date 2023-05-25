3574837bb9cd483a7f52a15aa2c060b8a56a7aef
Pedro Ferreira <pedrovictor48@live.com>
2023-05-24 23:00:08 -0300

    errinhos e prettier

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,12 @@
-const User = require("../models/user")
-const bcrypt = require("bcrypt")
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 const getUserInfo = async (req, res) => {
-	const {userId} = req.body
+	const { userId } = req.body
 
-	const user = await User.findOne({_id: userId}).select('name email cpf psic_id')
+	const user = await User.findOne({ _id: userId }).select(
+		'name email cpf psic_id'
+	)
 
 	console.log(user)
 
@@ -12,45 +14,43 @@ const getUserInfo = async (req, res) => {
 }
 
 const updateUser = async (req, res) => {
-	const {userId, newName, newEmail, cpf} = req.body
+	const { userId, newName, newEmail, cpf } = req.body
 
-	console.log("a")
-	console.log(req.body)
-
-	const user = await User.findOneAndUpdate({_id: userId},
-		{name: newName, email: newEmail, cpf}
+	await User.findOneAndUpdate(
+		{ _id: userId },
+		{ name: newName, email: newEmail, cpf }
 	)
-
 
 	res.sendStatus(200)
 }
 
 const changePassword = async (req, res) => {
-	const {userId, password, newPassword} = req.body
+	const { userId, password, newPassword } = req.body
 	console.log(newPassword)
 
 	const user = await User.findById(userId)
-	
-	if(await bcrypt.compare(password, user.password)) {
-		const hash = await bcrypt.hash(newPassword, 10);
-		await user.updateOne({password: hash})
+
+	if (bcrypt.compare(password, user.password)) {
+		const hash = await bcrypt.hash(newPassword, 10)
+		await user.updateOne({ password: hash })
 		return res.sendStatus(200)
-	}
-	else {
+	} else {
 		return res.sendStatus(403)
 	}
 }
 
 const getPsicUsers = async (req, res) => {
-	const {userId} = req.body
+	const { userId } = req.body
 
 	const user = await User.findById(userId)
 
-	if(user.__t != 'Psicologo') return res.sendStatus(403)
-	
-	const patients = await User.find({psic_id: userId}).select('name email cpf _id')
+	if (user.__t != 'Psicologo') return res.sendStatus(403)
+
+	const patients = await User.find({ psic_id: userId }).select(
+		'name email cpf _id'
+	)
 
 	return res.status(200).send(patients)
 }
 
-module.exports = {getUserInfo, updateUser, changePassword, getPsicUsers}
+module.exports = { getUserInfo, updateUser, changePassword, getPsicUsers }
