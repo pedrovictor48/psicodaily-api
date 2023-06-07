d260bd3f7e1b2139281d79d0fc069908dc6f9539
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-06-06 21:06:27 -0300

    test

--- index.js
+++ index.js
@@ -20,19 +20,21 @@ const auth = require("./routers/auth");
 const consulta = require("./routers/consulta");
 const user = require("./routers/user");
 const notif = require("./routers/notif");
-const mensagem = require("./routers/mensagem")
-const image = require("./controllers/imageController")
+const mensagem = require("./routers/mensagem");
+const image = require("./controllers/imageController");
 
 app.use(registro);
 app.use(auth);
 app.use(consulta);
-app.use(user)
-app.use(notif)
-app.use(mensagem)
-app.use(image)
+app.use(user);
+app.use(notif);
+app.use(mensagem);
+app.use(image);
 
-app.get('/', (req, res) => {
-	res.send("oi")
-})
-
+app.get("/", (req, res) => {
+  res.send("oi");
+});
+app.get("/ola", (req, res) => {
+  res.send("ola");
+});
 app.listen(port, () => console.log(`Server running at ${port}`));
