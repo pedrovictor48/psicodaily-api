0022f9b9c0fc8089a8b9be32c78cb037e62afd44
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-06-06 21:19:33 -0300

    fix

--- controllers/mensagemController.js
+++ controllers/mensagemController.js
@@ -1,61 +1,53 @@
-const Mensagem = require('../models/mensagem')
-const User = require('../models/user')
-const Paciente = require('../models/paciente')
+const Mensagem = require("../models/mensagem");
+const User = require("../models/user");
+const Paciente = require("../models/paciente");
 
 const getMensagens = async (req, res) => {
-	try {
-		const { userId } = req.body
-		const { pacienteId } = req.params
+  const { userId } = req.body;
+  const { pacienteId } = req.params;
 
-		const user = await User.findById(userId)
+  const user = await User.findById(userId);
 
-		if (user.__t == 'Psicologo') {
-			if (!pacienteId) return res.sendStatus(403)
-			const candidate = await Paciente.findOne({
-				_id: pacienteId,
-				psic_id: userId,
-			})
+  if (user.__t == "Psicologo") {
+    if (!pacienteId) return res.sendStatus(403);
+    const candidate = await Paciente.findOne({
+      _id: pacienteId,
+      psic_id: userId,
+    });
 
-			if (!candidate) return res.sendStatus(403)
+    if (!candidate) return res.sendStatus(403);
 
-			const messages = await Mensagem.find({
-				psicologoId: userId,
-				pacienteId: pacienteId,
-			})
-			return res.status(200).send(messages)
-		} else {
-			const messages = await Mensagem.find({
-				pacienteId: userId,
-				psicologoId: user.psic_id,
-			})
-			return res.status(200).send(messages)
-		}
-	} catch {
-		res.sendStatus(500)
-	}
-}
+    const messages = await Mensagem.find({
+      psicologoId: userId,
+      pacienteId: pacienteId,
+    });
+    return res.status(200).send(messages);
+  } else {
+    const messages = await Mensagem.find({
+      pacienteId: userId,
+      psicologoId: user.psic_id,
+    });
+    return res.status(200).send(messages);
+  }
+};
 
 const addMensagem = async (req, res) => {
-	try {
-		const { userId, receiverId, text, data } = req.body
-		const user = await User.findById(userId)
-		if (!user || user.__t != 'Psicologo') return res.sendStatus(403)
+  const { userId, receiverId, text, data } = req.body;
+  const user = await User.findById(userId);
+  if (!user || user.__t != "Psicologo") return res.sendStatus(403);
 
-		const candidate = await User.find({ _id: receiverId, psic_id: userId })
-		if (!candidate) return res.sendStatus(403)
+  const candidate = await User.find({ _id: receiverId, psic_id: userId });
+  if (!candidate) return res.sendStatus(403);
 
-		const mensagem = new Mensagem({
-			psicologoId: userId,
-			pacienteId: receiverId,
-			text,
-			senderName: user.name,
-			data: convertedDate,
-		})
-		await mensagem.save()
-		return res.sendStatus(200)
-	} catch {
-		return res.sendStatus(500)
-	}
-}
+  const mensagem = new Mensagem({
+    psicologoId: userId,
+    pacienteId: receiverId,
+    text,
+    senderName: user.name,
+    data: data,
+  });
+  await mensagem.save();
+  return res.sendStatus(200);
+};
 
-module.exports = { getMensagens, addMensagem }
+module.exports = { getMensagens, addMensagem };
